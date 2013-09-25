33defad1c4ed20d35e1550455d6bf8dfb4f04703
sconzen <sconzen@Stephans-iMac.local>
2013-09-25 13:29:40 -0400

    tempMsg box

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+jQuery(function($){
 
+	$('.tempMsg').hover(function(){
+		$(this).addClass('hover')
+	},function(){
+		$(this).removeClass('hover')
+	});
+	
+	$('.tempMsg').click(function(){
+		if($(this).hasClass('clicked')){
+			$(this).removeClass('clicked');
+		}else{
+			$(this).addClass('clicked');
+		}
+	});
+	
+});
