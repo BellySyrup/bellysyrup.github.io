d28a6561413780c97127c3796a2444dd90a81c12
sconzen <sconzen@Stephans-iMac.local>
2013-09-25 13:51:43 -0400

    took out stupid animation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 jQuery(function($){
 
-	$('.tempMsg').hover(function(){
-		$(this).addClass('hover')
-	},function(){
-		$(this).removeClass('hover')
-	});
-	
-	$('.tempMsg').click(function(){
-		if($(this).hasClass('clicked')){
-			$(this).removeClass('clicked');
-		}else{
-			$(this).addClass('clicked');
-		}
-	});
+	// do stuff
 	
 });
