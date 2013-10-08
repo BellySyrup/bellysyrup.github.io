2c059dcc7e769d52bda5eb29bc228c5fc8c11364
sconzen <sconzen@Stephans-iMac.local>
2013-10-08 12:55:02 -0400

    big changes

--- mobile/js/WoW.js
+++ mobile/js/WoW.js
@@ -1,6 +1,5 @@
 $(function(){
 	// create list of toons from array
-	console.log('WoW loaded');
 	var toons = new Array();
 	toons[0] = 'Helwyr';
 	toons[1] = 'Foongai';
@@ -11,25 +10,36 @@ $(function(){
 	toons[6] = 'Thorynn';
 	toons[7] = 'Baninii';
 	toons[8] = 'Runee';
+	toons[9] = 'Dornu';
+	toons[10] = 'Humpty';
 
-	// loop through toons array
 	$.each(toons,function(index,value){
-		// console.log(value);
 		checkToon(value);
-		$('#toons').append('<li><a href="http://us.battle.net/wow/en/character/jaedenar/'+value+'/simple"><img src="" id="'+value+'" alt="'+value+'"/>'+value+'</a><a href="http://www.guildox.com/wow/toon/us/Jaedenar/'+value+'"></a></li>')
+		$('#toons').append('<li id="'+value+'"><a href="http://us.battle.net/wow/en/character/jaedenar/'+value+'/simple"><img src="" id="" alt=""/><span>'+value+'</span></a><a href="http://www.guildox.com/wow/toon/us/Jaedenar/'+value+'"></a></li>');
 	});
-	 $("#list").listview("refresh");
 
-	 //http://us.battle.net/static-render/us/jaedenar/126/132626046-avatar.jpg Helwyr Thumbnail
-
-	 
+	$("#list").listview("refresh");
 	 
 });
 
 function foo(data) {
+
+	n = data.name;
+	l = data.level;
+	r = data.race;
+	g = data.gender;
+	rl = data.realm;
+	c = data.class;
+	t = 'http://us.battle.net/static-render/us/'+data.thumbnail;
+	ap = data.achievementPoints;
+	PC(c);
+
+	$this = $('#'+n);
+	$this.find('img').attr('src',t);
+	$this.find('span').html(n + ' - ' + l + ' ' + c);
+
 	console.log(data);
-	console.log("http://us.battle.net/static-render/us/"+data.thumbnail);
-	$('#'+data.name).attr('src','http://us.battle.net/static-render/us/'+data.thumbnail)
+
 }
  
 
@@ -40,3 +50,5 @@ function checkToon(name){
 	  dataType: 'jsonp'
 	});
 }
+
+function PC(e){switch(e){case 1:c="Warrior";break;case 2:c="Paladin";break;case 3:c="Hunter";break;case 4:c="Rogue";break;case 5:c="Priest";break;case 6:c="Death Knight";break;case 7:c="Shaman";break;case 8:c="Mage";break;case 9:c="Warlock";break;case 10:c="Monk";break;case 11:c="Druid";break}return c}
