6a72ad118f3f79fdbb5eb47fc4c13c8d9c47f2f7
sconzen <sconzen@Stephans-iMac.local>
2013-11-26 13:16:58 -0500

    123

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function footer(){
 	link1 = '<a href="http://sconzen.github.io/posts" id="posts">more</a>';
 	link2 = '<a href="http://sconzen.github.io/">uncopyright</a>';
 	link3 = '<a href="http://sconzen.github.io/">feed</a>';
-	link4 = '<a href="http://sconzen.github.io/">less</a>';
+	link4 = '<a href="http://sconzen.github.io/less.html">less</a>';
 	footer = link1 + ' :: ' + link2 + ' :: ' + link3 + ' :: ' + link4;
 	$('#footer').html('<p>'+footer+'</p>');
 }
