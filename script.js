d6cfe1adb09f1581bd2dd018beefe941637d044d
sconzen <sconzen@Stephans-iMac.local>
2013-11-26 12:22:41 -0500

    mnmlist

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function(){
+	title();
+	footer();
+});
+
+function title(){
+	console.log('title();');
+	title = 'mnmlist';
+	$('.post h2 a:first-child').html(title);
+}
+
+function footer(){
+	console.log('footer();');
+	link1 = '<a href="http://sconzen.github.io/posts/index.html" id="posts">more</a>';
+	link2 = '<a href="http://sconzen.github.io/">uncopyright</a>';
+	link3 = '<a href="http://sconzen.github.io/">feed</a>';
+	link4 = '<a href="http://sconzen.github.io/">less</a>';
+	footer = link1 + ' :: ' + link2 + ' :: ' + link3 + ' :: ' + link4;
+	$('#footer').html('<p>'+footer+'</p>');
+}
