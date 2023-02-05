35e21160c28080e34da754b7583dd5cd5827a9bb
Stephan <sconzen@gmail.com>
2023-02-05 18:28:44 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,10 +1,21 @@
 // Dashy Scripts
 // Grabs camera images every X seconds and refreshes embedded img
-setInterval(function() {
-	var c1 = document.getElementById('c1');
-	var c2 = document.getElementById('c2');
-	var c3 = document.getElementById('c3');
-		c1.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
-		c2.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
-		c3.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
-}, 30000);
+$(function() {
+    console.log( "ready!" );
+});
+
+
+// setInterval(function() {
+	// var c1 = document.getElementById('c1');
+	// var c2 = document.getElementById('c2');
+	// var c3 = document.getElementById('c3');
+		// c1.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
+		// c2.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
+		// c3.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
+// }, 30000);
+
+
+
+		// <img id="c1" src="http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11" style="width:100%;"/>
+         //   <img id="c2" src="http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11" style="width:100%;"/>
+          //  <img id="c3" src="http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11" style="width:100%;"/>
