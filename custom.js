e82795955656ef0267ce895867f0b9026a2fb9df
Stephan <sconzen@gmail.com>
2023-02-05 19:29:12 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,25 +1,34 @@
 // Dashy Scripts
-// Grabs camera images every X seconds and refreshes embedded img
-v = '0.1';
+v = '0.1.1';
 console.log('dashy scripts '+v);
+
+// Create images for each IP camara in wrapped div#c
+// refreshes every 30 seconds
+function camera(){
+	
+	var c = document.getElementById('c');	// get wrapper
+	c.innerHTML = '<img id="c1" src="http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11" style="width:100%;"/>'; // create camera img
+
+	setInterval(function(){
+		var c1 = document.getElementById('c1');
+		//var c2 = document.getElementById('c2');
+		//var c3 = document.getElementById('c3');
+		
+		c1.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
+		//c2.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
+		//c3.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
+		
+		c1.srcset = c1.src;
+		//c2.srcset = c2.src;
+		//c3.srcset = c3.src;
+		
+		console.log('refresh camera');
+	},30000);
+
+}
+
 console.log('load custom scripts');
-var c = document.getElementById('c');	// get wrapper
-var x = '<img id="c1" src="http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11" style="width:100%;"/>';
-c.innerHTML = x;
 
-setInterval(function(){
-	var c1 = document.getElementById('c1');
-	//var c2 = document.getElementById('c2');
-	//var c3 = document.getElementById('c3');
-	c1.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
-	c1.srcset = c1.src;
-	console.log('refresh camera' + c1.src);
-	//c2.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
-	//c3.src = 'http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11&rand=' + Math.random();
-},30000);
+camera();
 
-		// <img id="c1" src="http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11" style="width:100%;"/>
-         //   <img id="c2" src="http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11" style="width:100%;"/>
-          //  <img id="c3" src="http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11" style="width:100%;"/>
-		  
 console.log('end load custom scripts');
