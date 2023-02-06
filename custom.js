24f4e6d6c6a91a055273b8a226e4228720c553c1
Stephan <sconzen@gmail.com>
2023-02-05 19:56:33 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,14 +1,14 @@
 // Dashy Scripts
-v = '0.1.1';
+v = '0.1.2';
 console.log('dashy scripts '+v);
 
 // Create images for each IP camara in wrapped div#c
 // refreshes every 30 seconds
 function camera(){
-	
+	var s = 'style="width:100%;max-width:640px;"';
 	var c = document.getElementById('c');	// get wrapper
-	c.innerHTML = '<img id="c1" src="http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11" style="width:100%;"/>' +
-				 '<img id="c2" src="http://sconzen.github.io/images/banner.jpg" style="width:100%;"/>'; // create camera img
+	c.innerHTML = 	'<img id="c1" '+s+' src="http://admin:kermit101@192.168.50.11/snap.jpg?JpegCam=11"/>' +
+					'<img id="c2" '+s+' src="http://sconzen.github.io/images/banner.jpg"/>'; // create camera img
 
 	setInterval(function(){
 		var c1 = document.getElementById('c1');
@@ -23,7 +23,6 @@ function camera(){
 		//c2.srcset = c2.src;
 		//c3.srcset = c3.src;
 		
-		//console.log('refresh camera');
 	},30000);
 
 }
